880c2311033f29d717f8dd98913112f169ee248b
Vincent Pluchet <vpluchet@yahoo.fr>
2020-06-21 10:26:47 +0300

    Updating github

---
+++ separate.R
@@ -0,0 +1,115 @@
+library(tidyverse)
+library(dslabs)
+
+# import data
+path <- system.file("extdata", package = "dslabs")
+filename <- file.path(path, "life-expectancy-and-fertility-two-countries-example.csv")
+raw_dat <- read_csv(filename)
+select(raw_dat, 1:5)
+
+# gather all columns except country
+dat <- raw_dat %>% gather(key, value, -country)
+head(dat)
+dat$key[1:5]
+
+# separate on underscores
+dat %>% separate(key, c("year", "variable_name"), "_")
+dat %>% separate(key, c("year", "variable_name"))
+
+# split on all underscores, pad empty cells with NA
+dat %>% separate(key, c("year", "first_variable_name", "second_variable_name"), 
+                 fill = "right")
+
+# split on first underscore but keep life_expectancy merged
+dat %>% separate(key, c("year", "variable_name"), sep = "_", extra = "merge")
+
+# separate then spread
+dat %>% separate(key, c("year", "variable_name"), sep = "_", extra = "merge") %>%
+  spread(variable_name, value) 
+
+# separate then unite
+dat %>% 
+  separate(key, c("year", "first_variable_name", "second_variable_name"), fill = "right") %>%
+  unite(variable_name, first_variable_name, second_variable_name, sep="_")
+
+# full code for tidying data using the unite function
+dat %>% 
+  separate(key, c("year", "first_variable_name", "second_variable_name"), fill = "right") %>%
+  unite(variable_name, first_variable_name, second_variable_name, sep="_") %>%
+  spread(variable_name, value) %>%
+  rename(fertility = fertility_NA)
+
+# Read Marathon times.csv
+d <- read_csv("times.csv")
+marathon <- d %>% gather(year, time, -age_group)
+head(marathon)
+# converting back
+marathon %>% spread(year, time)
+
+# Read Diseases.txt
+d <- read_delim("Diseases.txt", delim = " ")
+d
+
+#Read Diseases.csv
+d <- read_csv("Diseases.csv")
+d
+tidy <- d %>% gather(key = disease, value = count, -state, -year, -population)
+head(tidy)
+
+# Read murders_tot
+d <- read_csv("murders_tot.csv")
+d
+tidy <- d %>% spread(key = var, value = people)
+tidy
+
+# Read 2015-2016 races
+d <- read_csv("2015-2016 races.csv")
+d
+gath <- d %>% gather(key = "key", value = "value", -age_group)
+head(gath)
+sep <- gath %>% separate(col = key, into = c("year", "variable_name"), sep = "_")
+head(sep)
+tidy <- sep %>% spread(key = variable_name, value = value)
+head(tidy)
+
+# Read basket stats
+stats <- read_csv("basket.csv")
+stats
+tidy <- stats %>%
+  separate(col = key, into = c("player", "variable_name"), sep = "_", extra = "merge") %>% 
+  spread(key = variable_name, value = value)
+tidy
+
+# Read CO2
+co2_wide <- data.frame(matrix(co2, ncol = 12, byrow = TRUE)) %>% 
+  setNames(1:12) %>%
+  mutate(year = as.character(1959:1997))
+head(co2_wide)
+
+co2_tidy <- co2_wide %>% gather(key = month, value = co2, -year)
+head(co2_tidy)
+
+co2_tidy %>% ggplot(aes(as.numeric(month), co2, color = year)) + geom_line()
+
+# Admissions
+data(admissions)
+head(admissions)
+dat <- admissions %>% select(-applicants)
+head(dat)
+
+# to get three columns major, men, women
+dat_tidy <- dat %>% spread(key = gender, value = admitted)
+head(dat_tidy)
+
+tmp <- gather(admissions, key, value, admitted:applicants)
+tmp
+
+tmp2 <- tmp %>% unite(col = column_name, c(key, gender), sep = "_")
+tmp2  
+
+# Reshape tmp2 to a table with six rows and five columns named major, admitted_men, 
+# admitted_women, applicants_men and applicants_women
+
+tmp3 <- tmp2 %>% spread(key = column_name, value = value)
+tmp3
+
---
+++ tidy.R
@@ -0,0 +1,55 @@
+library(tidyverse)
+library(dslabs)
+data(gapminder)
+
+# create and inspect a tidy data frame
+tidy_data <- gapminder %>% 
+  filter(country %in% c("South Korea", "Germany")) %>%
+  select(country, year, fertility)
+head(tidy_data)
+
+# plotting tidy data is simple
+tidy_data %>% 
+  ggplot(aes(year, fertility, color = country)) +
+  geom_point()
+
+# Plotting a list of countries
+country_sel = c("Germany", "France", "Italy")
+gapminder %>% filter(country %in% country_sel & !is.na(fertility)) %>% 
+  ggplot(aes(x = year, y = fertility, color = country)) +
+  geom_point() +
+  ggtitle("Fertility Rate per Country")
+
+# import and inspect example of original Gapminder data in wide format
+path <- system.file("extdata", package="dslabs")
+filename <- file.path(path,  "fertility-two-countries-example.csv")
+wide_data <- read_csv(filename)
+select(wide_data, country, `1960`:`1967`)
+
+# gather wide data to make new tidy data
+new_tidy_data <- wide_data %>%
+  gather(year, fertility, `1960`:`2015`)
+head(new_tidy_data)
+
+# gather all columns except country
+new_tidy_data <- wide_data %>%
+  gather(year, fertility, -country)
+
+# gather treats column names as characters by default
+class(tidy_data$year)
+class(new_tidy_data$year)
+
+# convert gathered column names to numeric
+new_tidy_data <- wide_data %>%
+  gather(year, fertility, -country, convert = TRUE)
+class(new_tidy_data$year)
+
+# ggplot works on new tidy data
+new_tidy_data %>%
+  ggplot(aes(year, fertility, color = country)) +
+  geom_point()
+
+# spread tidy data to generate wide data
+new_wide_data <- new_tidy_data %>% spread(year, fertility)
+select(new_wide_data, country, `1960`:`1967`)
+
