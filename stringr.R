cc5b41ea806545ba25947e03be593979e9de8acd
Vincent Pluchet <vpluchet@yahoo.fr>
2020-06-22 20:23:45 +0300

    examples of str_replace, str_detect, str_view, setNames,

---
+++ stringr.R
@@ -0,0 +1,125 @@
+library(tidyverse)
+library(rvest)
+
+# read in raw murders data from Wikipedia
+url <- "https://en.wikipedia.org/w/index.php?title=Gun_violence_in_the_United_States_by_state&direction=prev&oldid=810166167"
+murders_raw <- read_html(url) %>% 
+  html_nodes("table") %>% 
+  html_table() %>%
+  .[[1]] %>%
+  setNames(c("state", "population", "total", "murder_rate"))
+
+# inspect data and column classes
+head(murders_raw)
+class(murders_raw$population)
+class(murders_raw$total)
+
+# direct conversion to numeric fails because of commas
+murders_raw$population[1:3]
+as.numeric(murders_raw$population[1:3])
+
+# detect whether there are commas
+commas <- function(x) any(str_detect(x, ","))
+murders_raw %>% summarize_all(funs(commas))
+
+# replace commas with the empty string and convert to numeric
+test_1 <- str_replace_all(murders_raw$population, ",", "")
+test_1 <- as.numeric(test_1)
+
+# parse_number also removes commas and converts to numeric
+test_2 <- parse_number(murders_raw$population)
+identical(test_1, test_2)
+
+murders_new <- murders_raw %>% mutate_at(2:3, parse_number)
+murders_new %>% head
+str(murders_new)
+
+s <- "Hello!"    # double quotes define a string
+s <- 'Hello!'    # single quotes define a string
+# s <- `Hello`    # backquotes do not
+
+# s <- "10""    # error - unclosed quotes
+s <- '10"'    # correct
+
+# cat shows what the string actually looks like inside R
+cat(s)
+
+s <- "5'"
+cat(s)
+
+# to include both single and double quotes in string, escape with \
+# s <- '5'10"'    # error
+# s <- "5'10""    # error
+s <- '5\'10"'    # correct
+cat(s)
+s <- "5'10\""    # correct
+cat(s)
+
+# detect whether a comma is present
+pattern <- ","
+str_detect(murders_raw$total, pattern) 
+
+# show the subset of strings including "cm"
+str_subset(reported_heights$height, "cm")
+
+# use the "or" symbol inside a regex (|)
+yes <- c("180 cm", "70 inches")
+no <- c("180", "70''")
+s <- c(yes, no)
+str_detect(s, "cm") | str_detect(s, "inches")
+str_detect(s, "cm|inches")
+
+# highlight the first occurrence of a pattern
+str_view(s, pattern)
+
+# highlight all instances of a pattern
+str_view_all(s, pattern)
+
+# detect digits
+yes <- c("5", "6", "5'10", "5 feet", "4'11")
+no <- c("", ".", "five", "six")
+s <- c(yes, no)
+s
+pattern <- "\\d"
+str_detect(s, pattern)
+
+# highlight the first occurrence of a pattern
+str_view(s, pattern)
+
+# highlight all instances of a pattern
+str_view_all(s, pattern)
+
+# [56] means 5 or 6
+str_view(s, "[56]")
+
+# [4-7] means 4, 5, 6 or 7
+yes <- as.character(4:7)
+no <- as.character(1:3)
+s <- c(yes, no)
+str_detect(s, "[4-7]")
+str_view(s, "[4-7]")
+
+# ^ means start of string, $ means end of string
+pattern <- "^\\d$"
+yes <- c("1", "5", "9")
+no <- c("12", "123", " 1", "a4", "b")
+s <- c(yes, no)
+s
+str_view(s, pattern)
+
+# curly braces define quantifiers: 1 or 2 digits 
+pattern <- "^\\d{1,2}$"
+yes <- c("1", "5", "9", "12")
+no <- c("123", "a4", "b")
+str_view(c(yes, no), pattern)
+
+# combining character class, anchors and quantifier: beginning must be 4 to 7 with ' 
+# then 1-2 digits then " then the end
+pattern <- "^[4-7]'\\d{1,2}\"$"
+yes <- c("5'7\"", "6'2\"",  "5'12\"")
+no <- c("5'7", "6,2\"", "6.2\"","I am 5'11\"", "3'2\"", "64")
+cat(yes)
+str_detect(yes, pattern)
+cat(no)
+str_detect(no, pattern)
+
