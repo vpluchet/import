b1783a5b5c6576b53674b9cfbfd8174fa82cd39e
Vincent Pluchet <vpluchet@yahoo.fr>
2020-06-23 15:00:05 +0300

    testing patterns, str_trim, function converting to numbers, wrangling height file

--- heights.R
+++ heights.R
@@ -25,6 +25,38 @@ not_inches <- function(x, smallest = 50, tallest = 84){
   ind
 }
 
+# Testing the function
+sample <- c("48", "50", "52", "52\"", "52'", "164", "a", "ab")
+as.numeric(sample)
+not_inches(sample)
+not_inches(c(175))
+not_inches(c("5'8\""))
+not_inches(c(70))
+not_inches(c(85))
+
+# simple pattern
+s2 <- c("70", "5 ft", "4'11", "", ".", "Six feet")
+pattern_a <- "\\d|ft"
+pattern_b <- "\\d+|ft"
+pattern_c <- "\\d\\d|ft"
+pattern_d <- "\\d|feet"
+str_view_all(s2, pattern_a)
+str_view_all(s2, pattern_b)
+str_view_all(s2, pattern_c)
+str_view_all(s2, pattern_d)
+
+# simple pattern (continued)
+animals <- c("moose", "monkey", "meerkat", "mountain lion")
+pattern_e <- "mo*"
+pattern_f <- "mo?"
+pattern_g <- "mo+"
+pattern_h <- "moo*"
+str_detect(animals, pattern_e)
+str_detect(animals, pattern_f)
+str_detect(animals, pattern_g)
+str_detect(animals, pattern_h)
+
+
 # number of problematic entries
 problems <- reported_heights %>% 
   filter(not_inches(height)) %>%
@@ -89,4 +121,106 @@ problems %>%
   str_replace("inches|in|''|\"", "") %>% # remove all inches symbols
   str_detect(pattern) %>% 
   sum()
+
+# replacing ft by ft' 0
+yes <- c("5", "6", "5")
+no <- c("5'", "5''", "5'4")
+s <- c(yes, no)
+str_replace(s, "^([4-7])$", "\\1'0")
+str_replace(s, "^([56])'?$", "\\1'0") # to cover 5'
+
+pattern <- "^[4-7]\\s*'\\s*(\\d+\\.?\\d*)$"
+str_view(s, pattern)
+
+# replacing , by . but only in certain cases
+yes <- c("1,7", "1, 8", "2, " )
+no <- c("5,8", "5,3,2", "1.7")
+s <- c(yes, no)
+str_replace(s, "^([12])\\s*,\\s*(\\d*)$", "\\1\\.\\2")
+
+# trimming
+s <- "Hi "
+cat(s)
+identical(s, "Hi")
+str_trim(s)
+identical(str_trim(s), "Hi")
+
+# trim only trims at the beginning and the end
+str_trim(" 5 ' 9 ")
+
+# To upper and to lower case
+s <- c("Five feet eight inches")
+str_to_lower(s)
+
+# Creating a function
+convert_format <- function(s){
+  s %>%
+    str_replace("feet|foot|ft", "'") %>% #convert feet symbols to '
+    str_replace_all("inches|in|''|\"|cm|and", "") %>%  #remove inches and other symbols
+    str_replace("^([4-7])\\s*[,\\.\\s+]\\s*(\\d*)$", "\\1'\\2") %>% #change x.y, x,y x y
+    str_replace("^([56])'?$", "\\1'0") %>% #add 0 when to 5 or 6
+    str_replace("^([12])\\s*,\\s*(\\d*)$", "\\1\\.\\2") %>% #change european decimal
+    str_trim() #remove extra space
+}
+
+yes <- c("1,7", "1, 8", "2, " )
+no <- c("5,8", "5,3,2", "1.7")
+s <- c(yes, no)
+convert_format(s)
+
+# Function converting to numbers
+words_to_numbers <- function(s){
+  str_to_lower(s) %>%  
+    str_replace_all("zero", "0") %>%
+    str_replace_all("one", "1") %>%
+    str_replace_all("two", "2") %>%
+    str_replace_all("three", "3") %>%
+    str_replace_all("four", "4") %>%
+    str_replace_all("five", "5") %>%
+    str_replace_all("six", "6") %>%
+    str_replace_all("seven", "7") %>%
+    str_replace_all("eight", "8") %>%
+    str_replace_all("nine", "9") %>%
+    str_replace_all("ten", "10") %>%
+    str_replace_all("eleven", "11")
+}
+
+words_to_numbers(c(" five six seven"))
+
+converted <- problems %>% words_to_numbers %>% convert_format
+remaining_problems <- converted[not_inches_or_cm(converted)]
+pattern <- "^[4-7]\\s*'\\s*\\d+\\.?\\d*$"
+index <- str_detect(remaining_problems, pattern)
+remaining_problems[!index]
+
+
+
+# Putting it all together
+pattern <- "^([4-7])\\s*'\\s*(\\d+\\.?\\d*)$"
+smallest <- 50
+tallest <- 84
+new_heights <- reported_heights %>% 
+  mutate(original = height, 
+         height = words_to_numbers(height) %>% convert_format()) %>%
+  extract(height, c("feet", "inches"), regex = pattern, remove = FALSE) %>% 
+  mutate_at(c("height", "feet", "inches"), as.numeric) %>%
+  mutate(guess = 12*feet + inches) %>%
+  mutate(height = case_when(
+    !is.na(height) & between(height, smallest, tallest) ~ height, #inches 
+    !is.na(height) & between(height/2.54, smallest, tallest) ~ height/2.54, #centimeters
+    !is.na(height) & between(height*100/2.54, smallest, tallest) ~ height*100/2.54, #meters
+    !is.na(guess) & inches < 12 & between(guess, smallest, tallest) ~ guess, #feet'inches
+    TRUE ~ as.numeric(NA))) %>%
+  select(-guess)
+
+new_heights %>%
+  filter(not_inches(original)) %>%
+  select(original, height) %>% 
+  arrange(height) %>%
+  View()
+
+new_heights %>% arrange(height) %>% head(n=7)
+
+
+
 
